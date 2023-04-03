c05d37ecd9fcbb0d9fae909bc3d5734162aaab66
alejandro fleitas <afleitasinfo@gmail.com>
2023-04-03 00:58:19 -0300

    Added routing module for AuthComponent and configured in app-routing-module

--- Comic.Frontend/src/app/app-routing.module.ts
+++ Comic.Frontend/src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
     component: ErrorPageComponent
   },
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: '**',
-    component: ErrorPageComponent
+    redirectTo: '404'
   }
 ]
 
