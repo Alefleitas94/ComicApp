cac66f2c3f556bd7a1dbd10f022616b2fce057ec
alejandro fleitas <afleitasinfo@gmail.com>
2023-04-02 22:40:27 -0300

    C.R.U.D Character
    Added components for C.R.U.D Character Section

--- Comic.Frontend/src/app/character/character.module.ts
+++ Comic.Frontend/src/app/character/character.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroListComponent } from './pages/hero-list/hero-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddComponent,
+    SearchComponent,
+    CharacterComponent,
+    HomeComponent,
+    HeroListComponent
+  ],
   imports: [
     CommonModule
   ]
