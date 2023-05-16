ade5960c92b5e4f641b4899923ac1c5ff6c96e9d
alejandro fleitas <afleitasinfo@gmail.com>
2023-05-15 23:57:40 -0300

    Versioning DB
    Project DB was added to versionning

---
+++ Comic.DB/Comic.DB/Hero/Tables/Hero.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [hero].[heroes](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](100) NULL,
+	[gender] [varchar](10) NULL,
+	[publisher] [varchar](100) NULL,
+	[first_appearance] [varchar](100) NULL,
+	[created_at] [datetime2](7) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
